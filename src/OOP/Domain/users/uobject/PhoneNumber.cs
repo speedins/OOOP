cfb22caff8548b6fbe7074b2377208dd9b0aebd4
speedins <ladys2151@gmail.com>
2025-10-09 04:51:34 +0700

    Решение 3-4 задачи

---
+++ src/OOP/Domain/users/uobject/PhoneNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Domain.users.uobject
+{
+    internal record PhoneNumbers
+    {
+        public string PhoneNumber { get; }
+        private PhoneNumbers(string value)
+        {
+            PhoneNumber = value;
+        }
+        public static PhoneNumbers Create(string value)
+        {
+            Match match = Regex.Match(value, @"[8](\d{10})|[+7](\d{11})");
+            if (match.Success == false)
+            {
+                throw new ArgumentException("Ошибка в проверки маски телефона");
+            }
+            return new PhoneNumbers(value);
+        }
+    }
+}
